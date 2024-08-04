78326bdf0956fbc33c7e130b5f2420918976e2ff
Rakshit Dabral <96056758+rakshitdabral@users.noreply.github.com>
2024-08-04 14:48:20 +0530

    added border

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -30,7 +30,7 @@ function ContactForm() {
         id="name"
         type="text"
         name="to_name"
-        className="flex flex-col max-w-full text-base tracking-tight leading-none text-zinc-500 w-[500px] self-stretch px-6 py-5 w-full rounded min-h-[56px] max-md:px-5 max-md:max-w-full"
+        className="flex border-black border-2 rounded flex-col max-w-full text-base tracking-tight leading-none text-zinc-500 w-[500px] self-stretch px-6 py-5 w-full rounded min-h-[56px] max-md:px-5 max-md:max-w-full"
         placeholder="Your name"
       />
       <label htmlFor="email" className="sr-only">Email</label>
@@ -38,21 +38,21 @@ function ContactForm() {
         id="email"
         type="email"
         name="from_name"
-        className="flex flex-col mt-5 max-w-full text-base tracking-tight leading-none whitespace-nowrap text-zinc-500 w-[500px] self-stretch px-6 py-5 w-full rounded min-h-[56px] max-md:px-5 max-md:max-w-full"
+        className="flex border-black border-2 rounded flex-col mt-5 max-w-full text-base tracking-tight leading-none whitespace-nowrap text-zinc-500 w-[500px] self-stretch px-6 py-5 w-full rounded min-h-[56px] max-md:px-5 max-md:max-w-full"
         placeholder="Email"
       />
       <label htmlFor="website" className="sr-only">Your website (If exists)</label>
       <input
         id="website"
         type="url"
-        className="flex flex-col mt-5 max-w-full text-base tracking-tight leading-none text-zinc-500 w-[500px] self-stretch px-6 py-5 w-full rounded min-h-[56px] max-md:px-5 max-md:max-w-full"
+        className="flex border-black border-2 rounded flex-col mt-5 max-w-full text-base tracking-tight leading-none text-zinc-500 w-[500px] self-stretch px-6 py-5 w-full rounded min-h-[56px] max-md:px-5 max-md:max-w-full"
         placeholder="Your website (If exists)"
       />
       <label htmlFor="message" className="sr-only">How can I help?</label>
       <textarea
         id="message"
         name="message" 
-        className="flex flex-col mt-5 max-w-full text-base tracking-tight leading-5 text-zinc-500 w-[500px] flex-1 shrink px-6 py-4 w-full rounded min-h-[140px] max-md:px-5 max-md:max-w-full"
+        className="flex  border-black border-2 rounded flex-col mt-5 max-w-full text-base tracking-tight leading-5 text-zinc-500 w-[500px] flex-1 shrink px-6 py-4 w-full rounded min-h-[140px] max-md:px-5 max-md:max-w-full"
         placeholder="How can I help?*"
       ></textarea>
       <div className="flex flex-wrap gap-5 items-start mt-5 max-md:max-w-full">
