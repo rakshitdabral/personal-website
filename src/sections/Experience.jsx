fc3328de094aa555df63d0d0e214a7bf9f99f54e
Rakshit Dabral <rakshitdabral1@gmail.com>
2024-10-08 09:57:19 +0530

    updated

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,12 +1,20 @@
-import IBM from "../assets/ibm.svg";
+import IBM from "../assets/edunet.svg";
+import STUDIO from "../assets/studio.svg";
 import ExperienceCard from "../components/ExperienceCard";
 
 const experienceData = [
-    {
-    company: "IBM",
+  {
+    company: "Internship Studio",
     role: "Machine Learning Intern",
+    period: "August 2024 - September-2024",
+    description: "As a Machine Learning Engineer at Internship Studio, I gained valuable hands-on experience in the field. My internship focused on understanding and applying machine learning algorithms to real-world projects. I delved into various algorithms, learning their strengths and weaknesses. Additionally, I explored artificial neural networks and their applications. By the end of my internship, I successfully developed a YouTube ad view prediction model using an ANN, integrating the knowledge acquired throughout the program.",
+    logoSrc: STUDIO
+    },
+    {
+    company: "Edunet Foundation",
+    role: "Emerging Technologies (AI & Cloud)",
     period: "July 2024 - August-2024",
-    description: "During my one-month remote Machine Learning internship at IBM, specializing in WatsonX and Cloud Pak. Applied acquired knowledge to a Telecom Churn data analytics project. Leveraged IBM-provided datasets to construct a predictive model identifying factors contributing to customer churn.",
+    description: "During my one-month remote Machine Learning internship at Edunet Foundation, specializing in WatsonX and Cloud Pak. Applied acquired knowledge to a Telecom Churn data analytics project. Leveraged IBM-provided datasets to construct a predictive model identifying factors contributing to customer churn.",
     logoSrc: IBM
     }
 ]
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,32 +1,39 @@
 import ProjectCard from "../components/ProjectCard";
 import VariableWeightText from "../components/VariableWeightText";
 
-
-import Project1 from '../assets/project-1.png';
-import Project2 from '../assets/project-2.jpg';
+import Project1 from '../assets/project-1.jpg';
+import Project2 from '../assets/project-2.png';
 import Project3 from '../assets/project-3.jpg';
+import Project4 from '../assets/project-4.jpg';
 
 const projectsData = [
   {
     id: "01",
-    title: "Rooftop solar energy potential map",
-    description: "The Rooftop Solar Energy Potential Map project is a comprehensive initiative aimed at identifying and visualizing the potential for solar energy generation on rooftops within a specific geographic area. By leveraging advanced geospatial analysis and data-driven modeling, the project creates automatic solar energy consumption for the specific geographical area helping government decide where to plant solar planels",
-    link: "https://github.com/rakshitdabral/Rooftop-Solar-Energy-Potential-Map",
+    title: "Kisan Seva",
+    description: "An innovative app designed to empower farmers with advanced technology. Key features include real-time monitoring of soil conditions, automated irrigation based on machine learning predictions, access to up-to-date agricultural news, a personalized chatbot for farming advice, real-time weather updates, and AI-powered plant disease diagnosis through camera technology. This comprehensive app offers farmers a range of tools to optimize their farming practices and improve yields.",
+    link: "https://github.com/rakshitdabral/farmer-help",
     image: Project1,  
   },
   {
     id: "02",
-    title: "Sign Language Recognition System",
-    description: "This research project focuses on the development of a sign language recognition system employing a Convolutional Neural Network (CNN) architecture. Specifically, the project leverages the pre-trained VGG16 model through transfer learning to efficiently extract relevant features from image data and subsequently classify sign language gestures.",
-    link: "https://github.com/rakshitdabral/sign-language-recognition",
-    image: Project2,
+    title: "Rooftop solar energy potential map",
+    description: "The Rooftop Solar Energy Potential Map project is a comprehensive initiative aimed at identifying and visualizing the potential for solar energy generation on rooftops within a specific geographic area. By leveraging advanced geospatial analysis and data-driven modeling, the project creates automatic solar energy consumption for the specific geographical area helping government decide where to plant solar planels",
+    link: "https://github.com/rakshitdabral/Rooftop-Solar-Energy-Potential-Map",
+    image: Project2,  
   },
   {
     id: "03",
+    title: "Sign Language Recognition System",
+    description: "This research project focuses on the development of a sign language recognition system employing a Convolutional Neural Network (CNN) architecture. Specifically, the project leverages the pre-trained VGG16 model through transfer learning to efficiently extract relevant features from image data and subsequently classify sign language gestures.",
+    link: "https://github.com/rakshitdabral/sign-language-recognition",
+    image: Project3,
+  },
+  {
+    id: "04",
     title: "Sentiment Analysis on Web Fetched Articles",
     description: "This project is designed to conduct sentiment analysis on textual content extracted from a specified URL. To achieve this, the project leverages the Python programming language in conjunction with the requests library for retrieving article content and the nltk library for performing sentiment analysis.",
     link: "https://github.com/rakshitdabral/sentiment-analysis",
-    image: Project3,
+    image: Project4,
   }
 ]
 
